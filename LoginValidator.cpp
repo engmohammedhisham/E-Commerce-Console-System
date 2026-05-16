b9d5e896ca7b08a2b649de7a11402853a40348ce
Mohammed Hisham <s-mohammed.abdel-aziz@zewailcity.edu.eg>
2026-05-16 17:39:14 +0300

    Update LoginValidator.cpp

--- LoginValidator.cpp
+++ LoginValidator.cpp
@@ -1,11 +1,33 @@
 #include "LoginValidator.h"
+#include "FileManager.h"
 #include <iostream>
+#include <fstream>  
+#include <string>   
+
 using namespace std;
-bool LoginValidator::validateCustomerLogin(string username, string password) {
-    cout << "Validating Customer Login for: " << username << endl;
-    return true; 
-}
-bool LoginValidator::validateAdminLogin(string username, string password) {
-    cout << "Validating Admin Login for: " << username << endl;
-    return true;
+
+bool LoginValidator::validateCustomerLogin(string uname, string pass) {
+    ifstream file("D:\\my projects\\New folder\\final project\\x64\\users.txt");
+    string line;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+
+            size_t firstComma = line.find(',');
+            size_t lastComma = line.find_last_of(',');
+
+            if (firstComma != string::npos && lastComma != string::npos) {
+                string savedUname = line.substr(0, firstComma);
+                string savedPass = line.substr(firstComma + 1, lastComma - firstComma - 1);
+
+
+                if (uname == savedUname && pass == savedPass) {
+                    file.close();
+                    return true;
+                }
+            }
+        }
+        file.close();
+    }
+
+    return false;
 }
